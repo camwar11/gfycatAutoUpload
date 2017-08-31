11b11b78303f9a93ce8911d564d7cfe41164445c
Cam Warner <cameroncwarner@gmail.com>
2017-08-30 23:37:56 -0400

    [#2] Adding react

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="globals/electron-devtools-installer/index.d.ts" />
+/// <reference path="globals/electron/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/pug/index.d.ts" />
