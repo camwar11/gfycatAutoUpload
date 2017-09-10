318330809939bf113f4afdbe9bf17062686e1652
Cam Warner <cameroncwarner@gmail.com>
2017-09-09 22:02:52 -0400

    [#4] Adding electron-store

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="globals/electron-devtools-installer/index.d.ts" />
 /// <reference path="globals/electron/index.d.ts" />
+/// <reference path="globals/react-router-dom/index.d.ts" />
+/// <reference path="globals/react-router/index.d.ts" />
