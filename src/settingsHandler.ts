275cf6ff185bd79d8b05195e97a3185878bb7e5e
Cam Warner <cameroncwarner@gmail.com>
2017-09-09 22:37:43 -0400

    [#4] Adding electron store to save username and paths

--- src/settingsHandler.ts
+++ src/settingsHandler.ts
@@ -1,6 +1,7 @@
 import { ipcMain } from 'electron';
 import * as keyTar from 'keytar';
 import * as _ from 'lodash';
+import Store = require('electron-store');
 
 export const SETTINGS_CHANGED = 'settings-changed';
 export const GET_USERNAME = 'get-username';
@@ -25,16 +26,29 @@ interface IpcEvent {
 
 export class SettingsHandler {
     private readonly SERVICE_NAME = 'GfycatAutoUploader';
+    private readonly SETTINGS = 'settings';
+
     private _settings: GfycatClientSettings;
     private _listeners: Array<(val: GfycatClientSettings) => void>;
 
+    private _store: Store;
+
     constructor() {
         this._listeners = new Array<(val: GfycatClientSettings) => void>();
+        this._store = new Store();
+
         let self = this;
+
+        const savedSettings = this.retrieveSavedSettings();
+
+        if (savedSettings) {
+            this._settings = { ...savedSettings, password: this.getPassword };
+        }
 
         ipcMain.on(SETTINGS_CHANGED, (event: IpcEvent, arg: GfycatClientSettingsFromRender) => {
             console.log('settings changed');
             keyTar.setPassword(self.SERVICE_NAME, arg.userName, arg.password);
+            this._store.set(this.SETTINGS, {userName: arg.userName, paths: arg.paths});
             self._settings = { ...arg, password: self.getPassword};
             self._listeners.forEach((val) => {
                 val(self._settings);
@@ -44,6 +58,10 @@ export class SettingsHandler {
         ipcMain.on(GET_USERNAME, (event: IpcEvent, arg: any) => {
             event.returnValue = this._settings ? this._settings.userName : '';
         });
+    }
+
+    private retrieveSavedSettings() {
+        return this._store.get(this.SETTINGS) as SettingsBase;
     }
 
     getSettings(): GfycatClientSettings {
